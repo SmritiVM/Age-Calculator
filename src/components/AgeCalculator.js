60aa5488f6d16fa06e06fb457db31de91c6064fe
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-10-13 18:38:21 +0530

    Fixed errors + added styling

--- src/components/AgeCalculator.js
+++ src/components/AgeCalculator.js
@@ -1,5 +1,7 @@
 import {useState}from "react";
 
+import './AgeCalculator.css';
+
 export default function AgeCalculator(){
     const [birthDate, setBirthDate] = useState("");
     const [age, setAge] = useState("");
@@ -8,10 +10,21 @@ export default function AgeCalculator(){
         const currentDate = new Date();
         const selectedDate = new Date(birthDate);
 
+        if (selectedDate > currentDate){
+            setAge("invalid");
+            return;
+        }
+
+        
         if (!isNaN(currentDate) && !isNaN(selectedDate)){
             let yearDiff = currentDate.getFullYear() - selectedDate.getFullYear();
             let monthDiff = currentDate.getMonth() - selectedDate.getMonth();
             let dayDiff = currentDate.getDate() - selectedDate.getDate();
+
+            if (yearDiff === 0 && monthDiff === 0 && dayDiff === 0){
+                setAge("0. Happy Birthday!");
+                return;
+            }
 
             if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)){
                 yearDiff--;
@@ -25,13 +38,13 @@ export default function AgeCalculator(){
 
             let difference = "";
             if(yearDiff > 0){
-                difference += `${yearDiff} year${yearDiff > 1 ? "s":""}`;
+                difference += `${yearDiff} year${yearDiff > 1 ? "s ":" "}`;
             }
             if(monthDiff > 0){
-                difference += `${monthDiff} month${monthDiff > 1 ? "s":""}`;
+                difference += `${monthDiff} month${monthDiff > 1 ? "s ":" "}`;
             }
             if(dayDiff > 0){
-                difference += `${dayDiff} day${dayDiff > 1 ? "s":""}`;
+                difference += `${dayDiff} day${dayDiff > 1 ? "s ":" "}`;
             }
             setAge(difference);
         }
@@ -40,14 +53,17 @@ export default function AgeCalculator(){
         }
     };
     return(
-        <div>
+        <div className="container">
             <h1>Age Calculator</h1>
             <label htmlFor = "birthDate">Enter your date of birth</label>
+            <br/>
             <input 
             type = "date" id = "birthDate" value = {birthDate}
             onChange = {(e) => setBirthDate(e.target.value)}></input>
+            <br/>
             <button onClick = {calculateAge}>Calculate Age</button>
-            {age && <p>{`Your age is ${age}`}</p>}
+            <br/>
+            {age && <h3>{`You are ${age} old`}</h3>}
         </div>
     );
 }
